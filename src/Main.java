318e9e2a861a6c8a26c2774bd987409f6174c231
Ithan Camacho <ithan.camacho@epn.edu.ec>
2024-06-20 09:56:40 -0500

    operaciones basicas con GUI

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,14 @@ public class Main {
         int numero1 = Integer.parseInt(primernum);
         int numero2 = Integer.parseInt(segundonum);
         int suma = numero1 + numero2; // sumar numeros
+        int resta = numero1 - numero2; // sumar numeros
+        int multiplicacion = numero1 * numero2; // sumar numeros
+        int division = numero1 / numero2;
 
         JOptionPane.showMessageDialog(null, "El resultado de la suma es: " + suma,"Suma de dos enteros",JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null,"El resultado de la resta es: " + resta,"Resta de dos enteros",JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null,"El resultado de la multiplicacion es: " + multiplicacion,"Multiplicacion de dos enteros",JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null,"El resultado de la division es: " + division,"Division de dos enteros",JOptionPane.PLAIN_MESSAGE);
 
 
 
