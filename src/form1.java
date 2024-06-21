4ad34f5ef4c5105df5547ed893111276ba740f24
Ithan Camacho <ithan.camacho@epn.edu.ec>
2024-06-21 12:25:07 -0500

    calculadora simple

--- src/form1.java
+++ src/form1.java
@@ -1,8 +1,60 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class form1 {
 
 
-    private JButton btnOk;
+    private JButton multiplica;
     public JPanel mainPanel;
+    private JLabel nombre_lbl;
+    private JButton suma;
+    private JTextField num2;
+    private JButton divide;
+    private JTextField num1;
+    private JButton resta;
+
+    public form1() {
+        multiplica.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                float numero1 = Float.parseFloat(num1.getText());
+                float numero2 = Float.parseFloat(num2.getText());
+                float resultado = numero1 * numero2;
+                nombre_lbl.setText("Respuesta: "+ resultado);
+            }
+        });
+        suma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                float numero1 = Float.parseFloat(num1.getText());
+                float numero2 = Float.parseFloat(num2.getText());
+                float resultado = numero1 + numero2;
+                nombre_lbl.setText("Respuesta: "+ resultado);
+            }
+        });
+        divide.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                float numero1 = Float.parseFloat(num1.getText());
+                float numero2 = Float.parseFloat(num2.getText());
+                if (numero2 == 0) {
+                    nombre_lbl.setText("No se puede dividir para 0");
+                }else {
+                    float resultado = numero1 / numero2;
+                    nombre_lbl.setText("Respuesta: " + resultado);
+                }
+            }
+        });
+        resta.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                float numero1 = Float.parseFloat(num1.getText());
+                float numero2 = Float.parseFloat(num2.getText());
+                float resultado = numero1 - numero2;
+                nombre_lbl.setText("Respuesta: "+ resultado);
+            }
+        });
+    }
 }
